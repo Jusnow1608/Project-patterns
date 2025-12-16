985550aa2af0b1fd274db0378d4b9a850d80b9d2
Justyna <justyna.nowak.programista@gmail.com>
2025-12-16 18:45:19 +0100

    builder-end-internal-class

--- TreningWzorceProjektowe/src/co/devfoundry/patterns/builder/Main.java
+++ TreningWzorceProjektowe/src/co/devfoundry/patterns/builder/Main.java
@@ -5,7 +5,16 @@ import co.devfoundry.patterns.builder.house.House;
 public class Main {
     public static void main(String[] args)
     {
-House house1 = new House("walls", "floors", "rooms", "roof", "windows", "doors", "garage");
-House house2 = new House("walls", "floors", "roof");
+//House house1 = new House("walls", "floors", "rooms", "roof", "windows", "doors", "garage");
+//House house2 = new House("walls", "floors", "roof");
+
+        House house = new House.HouseBuilder()
+            .buildWalls("walls")
+                .buildFloors("floors")
+                .buildRoof("roof")
+                .buildRooms("rooms")
+                .build();
+
+        System.out.println(house);
+        }
     }
-}
