da2e9f881f21bf2ff18b084377610a4153573876
Justyna <justyna.nowak.programista@gmail.com>
2025-12-19 23:09:48 +0100

    flyweight-end

--- TreningWzorceProjektowe/src/co/devfoundry/patterns/flyweight/Main.java
+++ TreningWzorceProjektowe/src/co/devfoundry/patterns/flyweight/Main.java
@@ -2,7 +2,7 @@ package co.devfoundry.patterns.flyweight;
 
 import co.devfoundry.patterns.flyweight.units.Destroyer;
 import co.devfoundry.patterns.flyweight.units.Rifleman;
-import co.devfoundry.patterns.flyweight.units.TeslTank;
+import co.devfoundry.patterns.flyweight.units.TeslaTank;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +14,9 @@ public class Main {
 
     for (int i = 0; i<10000000; i++)
     {
-    activeUnits.add(new TeslTank("TeslaTank", 200,100,50,25,0,0, 200, 500));
-    activeUnits.add(new Rifleman("Rifleman", 25,100,50,25,0,0, 25,500));
-    activeUnits.add(new Destroyer("Destroyer", 200,100,50,25,0,0,200,300));
+    activeUnits.add(new TeslaTank(0,0));
+    activeUnits.add(new Rifleman(0,0));
+    activeUnits.add(new Destroyer(0,0));
     }
     }
 }
---
+++ TreningWzorceProjektowe/src/co/devfoundry/patterns/flyweight/units/UnitStatsRepository.java
@@ -0,0 +1,25 @@
+package co.devfoundry.patterns.flyweight.units;
+
+public class UnitStatsRepository {
+        private static UnitStats destroyerUnitStats = new UnitStats("Destroyer", 200,100,50,25,200);
+        private static UnitStats teslaTankUnitStats = new UnitStats("TeslaTank", 200,100,50,25, 200);
+        private static UnitStats riflemanUnitStats = new UnitStats ("Rifleman", 25,100,50,25, 500);
+
+        private UnitStatsRepository()   {  }
+
+    public static UnitStats getDestroyerStats ()
+        {
+            return destroyerUnitStats;
+        }
+
+    public static UnitStats getTeslaTankStats ()
+    {
+        return teslaTankUnitStats;
+    }
+
+    public static UnitStats getRiflemanStats ()
+    {
+        return riflemanUnitStats;
+    }
+}
+
