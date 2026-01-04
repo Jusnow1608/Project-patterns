b8ecc63c2db66bc6a1078192c26d7b98ee3397b5
Justyna <justyna.nowak.programista@gmail.com>
2026-01-04 19:58:08 +0100

    memento - task

---
+++ TreningWzorceProjektowe/src/co/devfoundry/patterns/mementotask/Main.java
@@ -0,0 +1,21 @@
+package co.devfoundry.patterns.mementotask;
+
+import co.devfoundry.patterns.mementotask.operating_system.OperatingSystem;
+import co.devfoundry.patterns.mementotask.operating_system.OperatingSystemCaretaker;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        OperatingSystemCaretaker operatingSystemCaretaker = new OperatingSystemCaretaker();
+        OperatingSystem operatingSystem = new OperatingSystem();
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+        operatingSystemCaretaker.addMemento(operatingSystem.save());
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+        operatingSystem.load(operatingSystemCaretaker.getMemento());
+    }
+}
---
+++ TreningWzorceProjektowe/src/co/devfoundry/patterns/mementotask/operating_system/OperatingSystem.java
@@ -0,0 +1,24 @@
+package co.devfoundry.patterns.mementotask.operating_system;
+
+import java.util.Date;
+
+public class OperatingSystem {
+
+    private int backupNumber = 0;
+    private Date backupDate;
+
+    public void createBackup(){
+        this.backupNumber++;
+        this.backupDate = new Date();
+        System.out.println("Utworzono backup nr: " + this.backupNumber + " z datą: " + backupDate);
+    }
+
+    public OperatingSystemMemento save(){
+        return new OperatingSystemMemento(this.backupNumber, this.backupDate);
+    }
+
+    public void load (OperatingSystemMemento memento){
+        this.backupNumber = memento.getBackupNumber();
+        this.backupDate = memento.getBackupDate();
+    }
+}
