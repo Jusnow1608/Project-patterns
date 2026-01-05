7a4b13847747f11cac9472aefc8c091a23ce5229
Justyna <justyna.nowak.programista@gmail.com>
2026-01-05 20:10:57 +0100

    state - start

---
+++ TreningWzorceProjektowe/src/co/devfoundry/patterns/state/Main.java
@@ -0,0 +1,13 @@
+package co.devfoundry.patterns.state;
+
+public class Main {
+
+    public static void main(String [] args)
+    {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        coffeeMachine.insertTheCoin();
+        coffeeMachine.pushTheButton();
+        coffeeMachine.takeTheCup();
+    }
+}
