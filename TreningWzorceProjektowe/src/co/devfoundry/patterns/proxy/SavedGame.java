6e3f2c4f66f924f592fdbdd4551727e0d0704d86
Justyna <justyna.nowak.programista@gmail.com>
2026-01-06 22:08:32 +0100

    proxy - end

--- TreningWzorceProjektowe/src/co/devfoundry/patterns/proxy/SavedGame.java
+++ TreningWzorceProjektowe/src/co/devfoundry/patterns/proxy/SavedGame.java
@@ -1,31 +1,10 @@
 package co.devfoundry.patterns.proxy;
 
-import static java.lang.Thread.sleep;
+public interface SavedGame {
 
-public class SavedGame {
+    public void initialize();
 
-    private String name;
-    private String gameData;
+    public void loadGame();
 
-    public void initialize(){
-        this.name = "Save Game - " + String.valueOf(System.currentTimeMillis());
-        this.gameData = loadFromStorage();
-    }
-
-    public void loadGame(){
-        System.out.println("Game loaded");
-    }
-
-    private String loadFromStorage(){
-        try{
-            sleep(500);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
-       return "Loaded";
-    }
-
-    public String getName(){
-        return this.name;
-    }
+    public String getName();
 }
